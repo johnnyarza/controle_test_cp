a6fdb0cdb8d361547710d452fb32f3e449e28d0e
johnnyarza <johnnyarza@gmail.com>
2020-08-07 17:47:52 -0300

    Fixed imports

--- src/main/java/gui/ReportConfigViewController.java
+++ src/main/java/gui/ReportConfigViewController.java
@@ -1,13 +1,10 @@
 package gui;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Path;
 import java.util.HashMap;
